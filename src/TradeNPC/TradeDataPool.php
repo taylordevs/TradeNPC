a7578f7f878ee8b4a67315384b7bc31015dd0964
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-06-03 11:31:32 +0700

    Format Docs

--- src/TradeNPC/TradeDataPool.php
+++ src/TradeNPC/TradeDataPool.php
@@ -1,10 +1,10 @@
 <?php
+
 declare(strict_types=1);
 
 namespace TradeNPC;
 
-class TradeDataPool
-{
+class TradeDataPool {
 
 	/** @var TradeNPC[] */
 	public static $interactNPCData = [];
--- src/TradeNPC/TradeNPC.php
+++ src/TradeNPC/TradeNPC.php
@@ -1,45 +1,38 @@
 <?php
+
 declare(strict_types=1);
 
 namespace TradeNPC;
 
 use pocketmine\entity\Human;
-use pocketmine\entity\NPC as PMNPC;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\item\Item;
 use pocketmine\nbt\LittleEndianNbtSerializer;
 use pocketmine\nbt\TreeRoot;
-use pocketmine\nbt\tag\ByteTag;
 use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\nbt\tag\IntTag;
-use pocketmine\nbt\tag\StringTag;
-use pocketmine\nbt\tag\ListTag;
-use pocketmine\nbt\tag\NameTag;
 use pocketmine\entity\Ageable;
 use pocketmine\entity\EntitySizeInfo;
 
-class TradeNPC extends Human implements Ageable
-{
-    
+class TradeNPC extends Human implements Ageable {
+
 	/** @var CompoundTag|null */
 	protected $shop = null;
-	
+
 	private $baby = false;
-	
-	protected function getInitialSizeInfo() : EntitySizeInfo{
+
+	protected function getInitialSizeInfo(): EntitySizeInfo {
 		return new EntitySizeInfo(1.8, 0.6); //TODO: eye height??
 	}
 
-	public function getName() : string{
+	public function getName(): string {
 		return "TradeNPC";
 	}
-    
-    public function isBaby() :bool {
-    	return $this->baby;
-    }
 
-	public function makeRecipe(Item $buyA, Item $buyB, Item $sell): CompoundTag
-	{
+	public function isBaby(): bool {
+		return $this->baby;
+	}
+
+	public function makeRecipe(Item $buyA, Item $buyB, Item $sell): CompoundTag {
 		$tag = CompoundTag::create()
 			->setTag("buyA", $buyA->nbtSerialize())
 			->setTag("buyB", $buyB->nbtSerialize())
@@ -51,55 +44,49 @@ class TradeNPC extends Human implements Ageable
 		return $tag;
 	}
 
-	public function addTradeItem(Item $buyA,Item $buyB, Item $sell): void
-	{
+	public function addTradeItem(Item $buyA, Item $buyB, Item $sell): void {
 		$this->getTagFunction($this->shop)->getListTag("Recipes")->push($this->makeRecipe($buyA, $buyB, $sell));
 	}
-    
-    public function getTagFunction($tag) {
-    	if($tag instanceof CompoundTag) {
-    	    $tag1 = $tag;
-        } elseif($tag instanceof TreeRoot){
+
+	public function getTagFunction($tag) {
+		if ($tag instanceof CompoundTag) {
+			$tag1 = $tag;
+		} elseif ($tag instanceof TreeRoot) {
 			$tag1 = $tag->getTag();
 		}
-        return $tag1;
-    }
-    
-	public function getShopCompoundTag()
-	{
-		if($this->shop instanceof CompoundTag){
+		return $tag1;
+	}
+
+	public function getShopCompoundTag() {
+		if ($this->shop instanceof CompoundTag) {
 			return $this->shop;
-		} elseif($this->shop instanceof TreeRoot){
+		} elseif ($this->shop instanceof TreeRoot) {
 			return $this->shop->mustGetCompoundTag();
 		}
 	}
 
-	public function saveNBT(): CompoundTag
-	{
+	public function saveNBT(): CompoundTag {
 		$nbt = parent::saveNBT();
 		Main::getInstance()->saveData($this);
 		return $nbt;
 	}
 
-	public function getSaveNBT(): string
-	{
+	public function getSaveNBT(): string {
 		return (new LittleEndianNbtSerializer)->write($this->getTreeRoot($this->shop));
 	}
 
-	public function loadData($nbt): void
-	{
+	public function loadData($nbt): void {
 		$this->shop = $nbt;
 	}
 	public function getTreeRoot($tag) {
-		if($tag instanceof CompoundTag) {
+		if ($tag instanceof CompoundTag) {
 			return new TreeRoot($tag);
-		} elseif($tag instanceof TreeRoot) {
+		} elseif ($tag instanceof TreeRoot) {
 			return $tag;
 		}
 	}
 
-	public function initEntity(CompoundTag $nbt): void
-	{
+	public function initEntity(CompoundTag $nbt): void {
 		parent::initEntity($nbt);
 		if ($this->shop === null) {
 			Main::getInstance()->loadData($this);
@@ -107,13 +94,11 @@ class TradeNPC extends Human implements Ageable
 		$this->setNameTagAlwaysVisible(true);
 	}
 
-	public function getTradeInventory(): TradeInventory
-	{
+	public function getTradeInventory(): TradeInventory {
 		return new TradeInventory($this);
 	}
 
-	public function attack(EntityDamageEvent $source): void
-	{
+	public function attack(EntityDamageEvent $source): void {
 		$source->cancel();
 	}
 }
